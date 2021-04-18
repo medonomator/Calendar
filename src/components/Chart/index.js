b73791d893c5d46b134601881fc9880e26b286af
Dmitry Zorin <dmitry.zorin@acmecryptocorp.io>
2021-04-18 08:22:53 +0300

    did toggler and button

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   LineChart,
   Line,
@@ -9,10 +9,45 @@ import {
 } from "recharts";
 import styles from "./chart.module.sass";
 
+import Ellipse from "../../assets/svg/ellipse.svg";
+
 const Chart = ({ numbers }) => {
+  const [toggler, changeToggler] = useState(0);
+
+  const toggle = (e) => {
+    changeToggler(!toggler);
+
+    console.log(toggler);
+
+    // if (toggler) {
+    //   e.target.style = "transform: translateX(50px)";
+    // } else {
+    //   e.target.style = "transform: translateX(0)";
+    // }
+  };
+
   return (
     <div className={styles.chartWrapper}>
-      <h4 className={styles.h4}>HABIT DEVELOPMENT</h4>
+      <div className={styles.chartTop}>
+        <h4 className={styles.h4}>HABIT DEVELOPMENT</h4>
+        <div className={styles.togglerWrap}>
+          <span>MONTH</span>
+          <div
+            className={styles.svgContainer}
+            onClick={() => changeToggler(!toggler)}
+          >
+            <img
+              style={{
+                right: toggler ? "-27px" : "0",
+              }}
+              className={styles.svg}
+              src={Ellipse}
+              alt="alt"
+            />
+          </div>
+          <span>YEAR</span>
+        </div>
+      </div>
       <LineChart width={800} height={400} data={numbers}>
         <XAxis
           tickLine={true}
@@ -24,6 +59,15 @@ const Chart = ({ numbers }) => {
         <Tooltip />
         <Line dataKey="pv" stroke="#f2c938" strokeWidth={3} />
       </LineChart>
+
+      <div className={styles.resetProgressCont}>
+        <button
+          className={styles.resetProgress}
+          onClick={() => alert("In development")}
+        >
+          Reset progress
+        </button>
+      </div>
     </div>
   );
 };
